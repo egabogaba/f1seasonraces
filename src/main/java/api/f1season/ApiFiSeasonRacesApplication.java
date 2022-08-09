aa99182a8b88517b0207b9fd749e278189fe2554
Efren Robles <efren_robles@hotmail.com>
2022-08-09 11:30:08 -0700

    !  Adding project base on springboot

---
+++ src/main/java/api/f1season/ApiFiSeasonRacesApplication.java
@@ -0,0 +1,13 @@
+package api.f1season;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFiSeasonRacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFiSeasonRacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/f1season/ApiFiSeasonRacesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.f1season;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFiSeasonRacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
