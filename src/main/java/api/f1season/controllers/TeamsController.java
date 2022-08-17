690b8bd7a2849465b76d7b611e829910d12f6d0a
gabo3pg <e.gabriel.gaba@gmail.com>
2022-08-17 10:13:32 -0500

    +Adding Api responses for swagger

--- src/main/java/api/f1season/controllers/TeamsController.java
+++ src/main/java/api/f1season/controllers/TeamsController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import api.f1season.entities.Teams;
 import api.f1season.services.TeamsService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/api/v1/teams")
@@ -22,6 +24,20 @@ public class TeamsController {
 	
 	@Autowired
 	private TeamsService teamsService;
+	
+	@ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Success"),
+                    @ApiResponse(responseCode = "202", description = "Accepted"),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Not Found"),
+                    @ApiResponse(responseCode = "405", description = "Invalid Input"),
+                    @ApiResponse(responseCode = "406", description = "Not Acceptable"),
+                    @ApiResponse(responseCode = "408", description = "Request Timeout"),
+                    @ApiResponse(responseCode = "415", description = "Unsupported Media Type"),
+                    @ApiResponse(responseCode = "500", description = "Failure"),
+                    @ApiResponse(responseCode = "501", description = "No Implemented")})
 	
 	@GetMapping(params = "teamId")
 	public Object getTeam(@PathParam(value = "teamId") Long teamId) {
@@ -33,17 +49,59 @@ public class TeamsController {
 		return teamsService.findTeam(teamId);
 	}
 
+	@ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Success"),
+                    @ApiResponse(responseCode = "202", description = "Accepted"),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Not Found"),
+                    @ApiResponse(responseCode = "405", description = "Invalid Input"),
+                    @ApiResponse(responseCode = "406", description = "Not Acceptable"),
+                    @ApiResponse(responseCode = "408", description = "Request Timeout"),
+                    @ApiResponse(responseCode = "415", description = "Unsupported Media Type"),
+                    @ApiResponse(responseCode = "500", description = "Failure"),
+                    @ApiResponse(responseCode = "501", description = "No Implemented")})
+	
 	@PostMapping
 	public Teams postTeam(@RequestBody Teams team) {		
 		
 		return teamsService.saveTeam(team);		
 	}
 	
+	@ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Success"),
+                    @ApiResponse(responseCode = "202", description = "Accepted"),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Not Found"),
+                    @ApiResponse(responseCode = "405", description = "Invalid Input"),
+                    @ApiResponse(responseCode = "406", description = "Not Acceptable"),
+                    @ApiResponse(responseCode = "408", description = "Request Timeout"),
+                    @ApiResponse(responseCode = "415", description = "Unsupported Media Type"),
+                    @ApiResponse(responseCode = "500", description = "Failure"),
+                    @ApiResponse(responseCode = "501", description = "No Implemented")})
+	
 	@PutMapping
 	public Teams putTeam(@PathParam(value = "teamId") Long teamId, @RequestBody Teams team) {
 		
 		return teamsService.updateEvent(teamId, team);		
 	}
+	
+	@ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Success"),
+                    @ApiResponse(responseCode = "202", description = "Accepted"),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Not Found"),
+                    @ApiResponse(responseCode = "405", description = "Invalid Input"),
+                    @ApiResponse(responseCode = "406", description = "Not Acceptable"),
+                    @ApiResponse(responseCode = "408", description = "Request Timeout"),
+                    @ApiResponse(responseCode = "415", description = "Unsupported Media Type"),
+                    @ApiResponse(responseCode = "500", description = "Failure"),
+                    @ApiResponse(responseCode = "501", description = "No Implemented")})
 	
 	@DeleteMapping
 	public void deleteEvent(@PathParam(value = "teamId") Long teamId) {
